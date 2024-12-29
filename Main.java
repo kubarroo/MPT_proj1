a3abf68cd5219360b83f59b59b5506b936e3836d
Jakub Szydełko <kubarroo248@interia.pl>
2024-12-30 00:14:13 +0100

    Main class modified

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+        PaymentProcessor processor = new PaymentProcessor();
+
+        processor.setPaymentMethod(new CreditCardPayment());
+        processor.processPayment(100.0);
+
+        processor.setPaymentMethod(new BankTransferPayment());
+        processor.processPayment(200.0);
     }
 }
